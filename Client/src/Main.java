2ada965cd1f46ec90d432c119e41b53b895e3b6f
Danidu Viraj <daniduijse65@Gmail.com>
2023-06-20 10:22:50 +0530

    commit

--- Client/src/Main.java
+++ Client/src/Main.java
@@ -1,5 +1,5 @@
-import java.io.DataOutputStream;
-import java.io.IOException;
+import java.io.*;
+import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Scanner;
 
@@ -7,11 +7,24 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
 
-        Socket socket = new Socket("localhost", 6666);
+        Socket socket = new Socket("localhost", 3002);
+        DataInputStream dataInputStream = new DataInputStream(socket.getInputStream());
         DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
-        dataOutputStream.writeUTF("Hello Server");
-        dataOutputStream.flush();
-        dataOutputStream.close();
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String message ="";
+        String reply = "";
+
+        while (!message.equals("Finish")){
+            message=dataInputStream.readUTF();
+            System.out.println(message);
+
+            reply=bufferedReader.readLine();
+            dataOutputStream.writeUTF(reply);
+
+            dataOutputStream.flush();
+            System.out.println(reply);
+        }
         socket.close();
+
     }
 }
--- Server/src/Main.java
+++ Server/src/Main.java
@@ -1,21 +1,30 @@
-import java.io.DataInputStream;
-import java.io.IOException;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Main {
     public static void main(String[] args) {
         try {
-            ServerSocket serverSocket = new ServerSocket(6666);
+            ServerSocket serverSocket = new ServerSocket(3002);
             Socket socket = serverSocket.accept();
+            System.out.println("HI");
 
             DataInputStream dataInputStream = new DataInputStream(socket.getInputStream());
+            DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+            String message ="";
+            String reply = "";
 
-            String message = dataInputStream.readUTF();
-            System.out.println("Client : "+message);
+            while (!message.equals("Finish")){
+                reply=bufferedReader.readLine();
+                dataOutputStream.writeUTF(reply);
+                dataOutputStream.flush();
+                System.out.println(reply);
 
+                message=dataInputStream.readUTF();
+                System.out.println(message);
+            }
             socket.close();
-
 
         } catch (IOException e) {
             throw new RuntimeException(e);
